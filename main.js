7a75d4c84667827fd5faeb2dd22c2b4e3ce610e9
medo94my <medoroyalrma@gmail.com>
2019-12-27 21:12:28 +0800

    until on screen control part 2

---
+++ main.js
@@ -0,0 +1,104 @@
+var GameState = {
+    init: function(){
+        //adapt the screen size, fit all the game
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageVertically = true;
+
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.physics.arcade.gravity.y = 1000;
+        
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.RUNNING_SPEED = 180;
+        this.JUMPING_SPEED = 550;
+    },
+    preload: function(){
+        this.load.image('actionButton', 'assets/images/actionButton.png');
+        this.load.image('arrowButton', 'assets/images/arrowButton.png');
+        this.load.image('barrel', 'assets/images/barrel.png');
+        this.load.image('goal', 'assets/images/gorilla3.png');
+        this.load.image('ground', 'assets/images/ground.png');
+        this.load.image('platform', 'assets/images/platform.png');
+
+        this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1 );
+        this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1);
+        
+    },
+    create: function(){
+        this.ground = this.add.sprite(0, 500, 'ground');
+        this.physics.arcade.enable(this.ground);
+        this.ground.body.allowGravity = false;
+        this.ground.body.immovable = true;
+
+        this.platform = this.add.sprite(0, 300, 'platform');
+        this.physics.arcade.enable(this.platform);
+        this.platform.body.allowGravity = false;
+        this.platform.body.immovable = true;
+        
+        this.player = this.add.sprite(100, 200, 'player', 3);
+        this.player.anchor.setTo(0.5);
+        this.physics.arcade.enable(this.player); 
+        this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
+        this.player.customParams = {};
+        this.createOnscreenControls();
+    },
+    update: function(){
+        this.game.physics.arcade.collide(this.player, this.ground);
+        this.game.physics.arcade.collide(this.player, this.platform);
+        
+        this.player.body.velocity.x = 0;
+
+        if(this.cursors.left.isDown || this.player.customParams.isMovingLeft){
+            this.player.body.velocity.x = -this.RUNNING_SPEED;
+        }
+        else if(this.cursors.right.isDown || this.player.customParams.isMovingRight){
+            this.player.body.velocity.x = +this.RUNNING_SPEED;
+        }
+
+        if((this.cursors.up.isDown  || this.player.customParams.mustJump) && this.player.body.touching.down){
+            this.player.body.velocity.y= -this.JUMPING_SPEED;
+        }
+        
+
+
+    },
+    createOnscreenControls: function(){
+        this.leftArrow = this.add.button(20, 535, 'arrowButton');
+        this.rightArrow = this.add.button(110, 535, 'arrowButton');
+        this.actionButton = this.add.button(280, 535, 'actionButton');
+
+        this.leftArrow.alpha = 0.5;
+        this.rightArrow.alpha = 0.5;
+        this.actionButton.alpha = 0.5;
+
+        this.actionButton.events.onInputDown.add(function(){
+            this.player.customParams.mustJump = true;
+        }, this);
+
+        this.actionButton.events.onInputUp.add(function(){
+            this.player.customParams.mustJump = false;
+        }, this);
+
+        this.leftArrow.events.onInputOver.add(function(){
+            this.player.customParams.isMovingLeft = true;
+        }, this);
+
+        this.leftArrow.events.onInputOut.add(function(){
+            this.player.customParams.isMovingLeft = false;
+        }, this);
+
+        this.rightArrow.events.onInputOver.add(function(){
+            this.player.customParams.isMovingRight = true;
+        }, this);
+
+        this.rightArrow.events.onInputOut.add(function(){
+            this.player.customParams.isMovingRight = false;
+        }, this);
+    }
+
+}
+
+var game = new Phaser.Game(360, 592, Phaser.AUTO);
+game.state.add('GameState', GameState);
+game.state.start('GameState');
