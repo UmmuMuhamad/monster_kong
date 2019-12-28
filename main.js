41091afdc2fd30a6269ad6d21eec68188e45dca4
Ummu Muhamad <amirah@kidocode.com>
2019-12-28 20:56:43 +0800

    until camera

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ var GameState = {
         
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.game.world.setBounds(0, 0, 360, 700);    
+
         this.RUNNING_SPEED = 180;
         this.JUMPING_SPEED = 550;
     },
@@ -26,26 +28,45 @@ var GameState = {
         
     },
     create: function(){
-        this.ground = this.add.sprite(0, 500, 'ground');
+        this.ground = this.add.sprite(0, 638, 'ground');
         this.physics.arcade.enable(this.ground);
         this.ground.body.allowGravity = false;
         this.ground.body.immovable = true;
 
-        this.platform = this.add.sprite(0, 300, 'platform');
-        this.physics.arcade.enable(this.platform);
-        this.platform.body.allowGravity = false;
-        this.platform.body.immovable = true;
-        
-        this.player = this.add.sprite(100, 200, 'player', 3);
+        var platfromData = [
+            
+            {"x": 0, "y": 430},
+            {"x": 45, "y": 560},
+            {"x": 90, "y": 290},
+            {"x": 0, "y": 140}
+            
+            
+        ]
+
+        this.platforms = this.add.group();
+        this.platforms.enableBody = true;  
+
+        platfromData.forEach(function(element){
+            this.platforms.create(element.x, element.y, 'platform');
+        }, this);
+
+        this.platforms.setAll('body.immovable', true);
+        this.platforms.setAll('body.allowGravity', false);
+            
+
+        this.player = this.add.sprite(10, 545, 'player', 3);
         this.player.anchor.setTo(0.5);
         this.physics.arcade.enable(this.player); 
         this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
         this.player.customParams = {};
+
+        this.game.camera.follow(this.player);
+
         this.createOnscreenControls();
     },
     update: function(){
         this.game.physics.arcade.collide(this.player, this.ground);
-        this.game.physics.arcade.collide(this.player, this.platform);
+        this.game.physics.arcade.collide(this.player, this.platforms);
         
         this.player.body.velocity.x = 0;
 
@@ -71,6 +92,10 @@ var GameState = {
         this.leftArrow.alpha = 0.5;
         this.rightArrow.alpha = 0.5;
         this.actionButton.alpha = 0.5;
+
+        this.leftArrow.fixedToCamera = true;
+        this.rightArrow.fixedToCamera = true;
+        this.actionButton.fixedToCamera = true;
 
         this.actionButton.events.onInputDown.add(function(){
             this.player.customParams.mustJump = true;
